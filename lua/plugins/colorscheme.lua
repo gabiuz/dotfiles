f89a7e3cec657f733aaa535f531d246122c3798c
gabiuz <pelagionicolai@gmail.com>
2025-03-28 17:46:25 +0800

    changed theme to gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,12 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    style = "moon",
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
     },
   },
 }
